349a74404e5829d5645efa56836842461d86a903
gytrhu6u-g-2 <gytrhu6u@icloud.com>
2023-06-20 02:28:30 +0900

    listの点線消す

--- resources/views/index/top.blade.php
+++ resources/views/index/top.blade.php
@@ -11,14 +11,14 @@
     <div class="container">
         <div class="screen-container">
             @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             <div class="input-clear-add-container">
                 <form action="{{ route('store') }}" method="POST">
                     @csrf
